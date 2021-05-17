3f8f1d53221116d09a3857d11401bd73ded5cf58
Yuqiu.He <yuqiu@igola.com>
2021-05-17 14:44:28 +0800

    init

---
+++ 数据结构/data.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("===================== arrayFunc")
+	arrays()
+
+}
+
+// 数组
+func arrays() {
+	var test1 [6]int
+	fmt.Println("内容：", test1)
+}
