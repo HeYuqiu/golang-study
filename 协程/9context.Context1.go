b4a97e99fef15d1669e7180a7291e881e446f2c2
Yuqiu.He <375495898@qq.com>
2021-06-09 14:27:53 +0800

    context.Context

---
+++ 协程/9context.Context1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// ctx，类似线上上下文，当前协程和子协程共享；当前协程对ctx的操作，所有子协程都能感知到
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go watch(ctx, "【监控1】")
+	go watch(ctx, "【监控2】")
+	go watch(ctx, "【监控3】")
+	time.Sleep(10 * time.Second)
+	fmt.Println("可以了，通知监控停止")
+	cancel()
+	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+	time.Sleep(5 * time.Second)
+}
+
+func watch(ctx context.Context, name string) {
+	for {
+		select {
+		case <-ctx.Done(): // 每个协程都会通知到
+			fmt.Println(name, "监控退出，停止了...")
+			return
+		default:
+			fmt.Println(name, "goroutine监控中...")
+			time.Sleep(2 * time.Second)
+		}
+	}
+}
