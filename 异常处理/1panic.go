d8d2d0c1c907af2e67098e206918aba65b184bc3
Yuqiu.He <heyuqiu.hawk@bytedance.com>
2021-08-24 11:58:05 +0800

    空指针 panic

--- 异常处理/1panic.go
+++ 异常处理/1panic.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+// panic 表示进程内的错误。panic 的原因来自于代码的逻辑 bug，比如强制类型转换失败，比如数组越界。这个代表了程序员的责任不到位，导致了程序的panic。 error 代表进程外的错误。比如输入符合预期。比如访问外部的服务失败。这些都不是程序员可以设计控制的。这些情况的错误处理是业务逻辑的一部分。
+// Go中可以抛出一个panic的异常，然后在defer中通过recover捕获这个异常，然后正常处理。
 func main() {
-	panic("a problem")
+
+	defer func() { // 必须要先声明defer，否则不能捕获到panic异常
+		fmt.Println("c")
+		if err := recover(); err != nil {
+			fmt.Println(err) // 这里的err其实就是panic传入的内容，55
+		}
+		fmt.Println("d")
+	}()
+
+	f()
+}
+
+func f() {
+	fmt.Println("a")
+	panic(55)
+	fmt.Println("b")
+	fmt.Println("f")
 }
--- 指针/4string指针.go
+++ 指针/4string指针.go
@@ -2,11 +2,21 @@ package main
 
 import "fmt"
 
+type person struct {
+	name *string
+}
+
 func strr(str *string) {
 	fmt.Println(str)
 }
 
 func main() {
+	//name := "name"
+	p := &person{
+		//name: &name,
+	}
+	//fmt.Println("hyq" + p.name)
+	fmt.Println("hyq" + *p.name)
 
 	str := "fff"
 	//strr(str) // 报错
